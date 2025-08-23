7760b8f1c1f21229ecb5a50251dd7d4a1afc12b6
jms1999lsc <joaomsilva7@gmail.com>
2025-08-24 00:41:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -193,7 +193,7 @@ def make_unique(columns):
 st.sidebar.markdown(
     """
     <div style="display:flex; align-items:center; justify-content:center; margin-top:-40px; margin-bottom:10px;">
-        <img src="logo.png" width="50" style="margin-right:10px;">
+        <img src="logo.png" width="120" style="margin-right:10px;">
         <h3 style="margin:0; color:#bd0003;">Leixões SC</h3>
     </div>
     """,
@@ -680,6 +680,7 @@ if preset_up:
         st.sidebar.success("Preset carregado (aplica manualmente as escolhas na UI).")
     except Exception as e:
         st.sidebar.error(f"Preset inválido: {e}")
+
 
 
 
